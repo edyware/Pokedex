1c5b8bdc2f1e336368de97630e7eba3a61877162
Edyware <ede_ceni@hotmail.com>
2022-01-19 12:09:31 -0300

    Criação da lista de pokemons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaPokemonsComponent } from './lista-pokemons/lista-pokemons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaPokemonsComponent
   ],
   imports: [
     BrowserModule,
